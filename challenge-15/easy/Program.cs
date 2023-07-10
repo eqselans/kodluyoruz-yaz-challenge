db4308adddc551425ae55dab511e0b4d95de769b
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-07-11 01:43:37 +0300

    Proje oluşturuldu

--- challenge-15/easy/Program.cs
+++ challenge-15/easy/Program.cs
@@ -10,7 +10,37 @@ namespace easy
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Dogum gününüz:");
+            int dogumGunu = Convert.ToInt32(Console.ReadLine());
+            // Kullanıcıdan doğum günü alındı
+
+            Console.Write("Dogum ayınız:");
+            int dogumAyi = Convert.ToInt32(Console.ReadLine());
+            // Kullanıcıdan doğum ayı alındı
+
+            Console.Write("Dogum yılınız:");
+            int dogumYili = Convert.ToInt32(Console.ReadLine());
+            // Kullanıcıdan doğum yılı alındı
+
+            DateTime dogumTarihi = new DateTime(dogumYili,dogumAyi,dogumGunu);
+
+            DateTime bugun = DateTime.Today;
+
+
+            int yas = bugun.Year - dogumTarihi.Year;
+            // Yıl farkları yapıldı
+
+
+            if (dogumTarihi > bugun.AddYears(-yas) )
+            {
+                // Eğer doğum tarihi gün ve ay bakımından bugünün yaş yıl kadar öncesinden büyükse
+                // yaşın bir değer azaltılması sağlandı
+
+                yas--;
+            }
+            Console.Write("Yaşınız {0}",yas);
+
+            Console.ReadLine();
         }
     }
 }
