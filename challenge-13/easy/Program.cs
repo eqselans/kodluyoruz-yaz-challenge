0f35d4e1bb0b62efb996e87248b00a03949f575a
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-07-27 00:44:20 +0300

    Challenge 13 easy ve medium projeleri yapıldı.

---
+++ challenge-13/easy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz : ");
+            
+            int say = Convert.ToInt32(Console.ReadLine());
+
+            int sayac = 0;
+
+            for (int i = 1; i < say; i++)
+            {
+                
+                if (say % i == 0)
+                {
+                    sayac++;
+                       
+                }
+            }
+            if (sayac == 1)
+            {
+                Console.WriteLine("Bu bir asal sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("Bu bir asal sayı değildir");
+            }
+        
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ challenge-13/medium/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen büyütmek istediğiniz kelimeyi giriniz: ");
+            try
+            {
+                string kelime = Console.ReadLine();
+
+                Console.WriteLine("Büyütülmüş kelime: " + kelime.ToUpper());
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Lütfen string türünde bir değer giriniz!");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
