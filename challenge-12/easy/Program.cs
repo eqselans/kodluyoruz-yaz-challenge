88dddb000840493b11d68b64ff616ac558e6f97b
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-08-22 13:02:00 +0300

    challenge-12 oluşturuldu.

---
+++ challenge-12/easy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hız = 60;
+            int yol = 240;
+
+            Console.WriteLine("Aracın {0} km/h hızla {1} km yol gidebilmesi için {2} saat gereklidir", hız, yol, yol / hız);
+            Console.ReadLine();
+        }
+    }
+}
