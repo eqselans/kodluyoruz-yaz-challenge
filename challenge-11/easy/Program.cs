afdec37b64096bfcbf4416c4f0a4977878e8e63f
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-09-07 14:19:03 +0300

    challenge-11 oluşturuldu ve easy projesi eklendi

---
+++ challenge-11/easy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hesaplanacak sayıyı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int faktoriyel =1;
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel *= i;
+            }
+
+            Console.WriteLine("{0} ! = {1}",sayi, faktoriyel);
+            Console.ReadLine();
+
+        }
+    }
+}
