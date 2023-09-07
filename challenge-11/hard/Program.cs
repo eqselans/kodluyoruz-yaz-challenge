931ca0a2577d6285417500cbce0ab88a7a1c51f8
Emirhan AKSU <emirhanaksu1903@gmail.com>
2023-09-07 14:39:53 +0300

    hard projesi oluşturuldu.

---
+++ challenge-11/hard/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] siralanmisDizi = { 2, 3, 3, 5, 5, 5, 7, 8, 8, 8, 10 };
+
+
+            Console.Write("Hedef sayı: ");
+            int hedefSayi = Convert.ToInt32(Console.ReadLine());
+
+            int tekrar = 0;
+
+            foreach(int i in siralanmisDizi)
+            {
+                if (i == hedefSayi)
+                {
+                    tekrar++;
+                }
+                if (i>hedefSayi)
+                {
+                    break;
+                }
+            }
+            Console.Write("Tekrar sayısı: "+ tekrar);
+            Console.ReadLine();
+        }
+    }
+}
